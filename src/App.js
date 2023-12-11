aa5e4e6b11006a7a56312ec978690896c470290d
Tohan-B <136444891+Tohan-B@users.noreply.github.com>
2023-12-11 06:14:01 -0800

    Created footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <footer>
+        <small class="author-info">
+          <a
+            href="https://github.com/Tohan-B/SheCodes-Weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>
+          , by Blessing Itohan
+        </small>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
