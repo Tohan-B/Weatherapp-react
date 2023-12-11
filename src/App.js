bdd07b552ae8b16e1c4636ef0b4027bab690ab04
Tohan-B <136444891+Tohan-B@users.noreply.github.com>
2023-12-11 06:29:51 -0800

    Created WeatherApp component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import "./App.css";
+import WeatherApp from "./WeatherApp";
 
 function App() {
   return (
-    <div className="App">
-      <footer>
-        <small class="author-info">
-          <a
-            href="https://github.com/Tohan-B/SheCodes-Weather-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Open-source code
-          </a>
-          , by Blessing Itohan
-        </small>
-      </footer>
+    <div className="container">
+      <div className="weather-app">
+        <WeatherApp />
+        <footer>
+          <small class="author-info">
+            <a
+              href="https://github.com/Tohan-B/SheCodes-Weather-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open-source code
+            </a>
+            , by Blessing Itohan
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
